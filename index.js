89bfe55a768f6e213f3a48599c6fdeaf9eb19310
pulvariser <mayurburange72@gmail.com>
2024-10-04 18:14:59 +0530

    cors fixes

--- index.js
+++ index.js
@@ -9,9 +9,13 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({
-    origin: ['https://myport-blush.vercel.app'], // Allow only this origin
-    methods: ['GET', 'POST'], // Specify allowed methods
+    origin: 'https://myport-blush.vercel.app',  // Replace with your frontend URL
+    methods: ['GET', 'POST'],  // Allow only the necessary methods
+    credentials: true,  // If you're sending cookies or other credentials
   }));
+
+app.options('*', cors());  // Allow preflight requests for all routes
+
 app.use(express.json());  // Parses incoming JSON requests
 
 // Routes
